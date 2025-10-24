0d6588f17077bd99b79f8383ccc5e8d9b620b2bb
habib4445 <0124habibur@gmail.com>
2025-10-24 19:37:33 +0600

    parse int

--- number.js
+++ number.js
@@ -3,5 +3,11 @@
 // console.log( mobilePrice);
 // console.log(typeof headphone);
 // console.log(mobilePrice+headphone);
- var apple=parseInt('22.33');
- console.log(apple);
+//  var apple=parseInt('22.33');
+//  console.log(apple);
+var f=0.1;
+var s=0.2;
+var total=f+s;
+console.log(total.toFixed(3));
+const num = 50/0;
+console.log(num)
