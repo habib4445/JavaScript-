416c9625227b5660e4339bb5bc52bd58d79bc361
habib4445 <0124habibur@gmail.com>
2025-10-24 18:53:55 +0600

    new

--- number.js
+++ number.js
@@ -1,4 +1,7 @@
-var mobilePrice=5500;
-var headphone=30.23;
-console.log( mobilePrice);
-console.log(typeof headphone);
+// var mobilePrice=5500;
+// var headphone=20;
+// console.log( mobilePrice);
+// console.log(typeof headphone);
+// console.log(mobilePrice+headphone);
+ var apple=parseInt('22.33');
+ console.log(apple);
